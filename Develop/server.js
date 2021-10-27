be7ac873a31315dd35b6a1d0c73a126f9c81e35e
Dylan Dimayuga <dylandimayuga@yahoo.com>
2021-10-27 11:20:35 -0500

    added code to the server.js file so that it will take the user's input and save it. Unable to get it to pull up any of the notes on the side to be viewed.

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const path = require('path');
 const fs = require('fs');
-const uuid = require('./Develop/helpers/uuid');
+const notes = require('./db/db.json');
+const uuid = require('./helpers/uuid');
 const PORT = 3001;
 const app = express();
 
@@ -10,7 +12,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
-app.get('/api/nots')
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'))
+})
+
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'))
+    const newNotes = req.body;
+    newNotes.id = uuid
+    notes.push(newNotes)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes);
+})
+
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+})
